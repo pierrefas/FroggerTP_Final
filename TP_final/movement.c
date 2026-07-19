d4bcebe9b1983f8f639e5d6dd47ac7e90d5cce9a
Diabetes-wheely <sdeabate@itba.edu.ar>
2026-07-18 21:17:18 -0300

    soygoingui

--- TP_final/movement.c
+++ TP_final/movement.c
@@ -2,25 +2,25 @@
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_primitives.h>
+void movement ()
+    extern frog;
 
-extern frog;
-
-if (event.keyboard.keycode == ALLEGRO_KEY_W || /*joystick*/)  //se mueve para arriba
-{
+    if (event.keyboard.keycode == ALLEGRO_KEY_W || /*joystick*/)  //se mueve para arriba
+    {
     frogStepUp(frog);
-}
-if (event.keyboard.keycode == ALLEGRO_KEY_s || /*joystick*/)
-{
+    }
+    if (event.keyboard.keycode == ALLEGRO_KEY_s || /*joystick*/)
+    {
     frogStepDown(frog);
-}
-if (event.keyboard.keycode == ALLEGRO_KEY_D || /*joystick*/)
-{
+    }
+    if (event.keyboard.keycode == ALLEGRO_KEY_D || /*joystick*/)
+    {
     frogStepRight(frog);
-}
-if ((event.keyboard.keycode == ALLEGRO_KEY_A)|| /*joystick*/)
-{
+    }
+    if ((event.keyboard.keycode == ALLEGRO_KEY_A)|| /*joystick*/)
+    {
     frogStepLeft(frog);
-}
+    }
 //hay que hacer un IF(entra movimiento direccional (4 casos) por allegro teclado o por joystick){ 
 // llame a la funcion de movimiento adecuada, levantando su flag asociado
 // haciendo que la funcion asociada ocurra, en caso allegro cambie el sprite y en disp que cambie
