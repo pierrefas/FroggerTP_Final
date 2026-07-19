033eeaddd188ca7972a7c464c74c476976e492f4
Tocadoxmitio <matiaslp.1805@gmail.com>
2026-07-19 00:25:21 +0000

    creo que soy gay

--- TP_final/joystick.c
+++ TP_final/joystick.c
@@ -9,24 +9,47 @@
 #include "entidades.h"
 #include "frogupdates.h"
 #include "joydrv.h"
-#define DEADZONE 100 // zona muerta del joystick
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int main (void){
+int main (void)
 
-    {
-        
+    {   
+        int isActive; 
         joy_init();
         joyinfo_t data = {0,0,J_NOPRESS};
     
     do
         {
         data = joy_read();
-        if (data.x > DEADZONE){
-            frogStepRight()
-        }
-        if (data.x < - DEADZONE){}
-        if (data.y > DEADZONE){}
-        if (data.y < -DEADZONE){}
+        if (abs(data.x)<DEADZONE && abs(data.x) < DEADZONE)
+            {
+                isActive = 0;
+            }
+        if(!isActive) 
+            { 
+            if (data.x > DEADZONE)
+                {
+                    isActive = 1;
+                    printf("El jugador se movió a la derecha");
+                }
+            if (data.x < - DEADZONE)
+                {
+                  isActive = 1;
+                    printf("El jugador se movió a la izquierda");
+                }
+            if (data.y > DEADZONE)
+                {
+                    isActive = 1;
+                    printf("El jugador se movió arriba");
+                }
+        if (data.y < -DEADZONE)
+                {
+                    isActive = 1;
+                    printf("El jugador se movió abajo");
+                }
+            }
         }
         while(data.sw == J_NOPRESS);
 }
--- TP_final/joystick.h
+++ TP_final/joystick.h
@@ -1,6 +1,6 @@
 #ifndef JOYSTICK_H
 #define JOYSTICK_H
-
-//header del joistick (˶>⩊<˶)
+#define DEADZONE 100 // zona muerta del joystick
+int isActive;
 
 #endif
