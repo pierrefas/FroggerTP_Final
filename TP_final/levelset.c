5f889f679b2053d8809c27ee7674787d17e93d5c
Diabetes-wheely <sdeabate@itba.edu.ar>
2026-07-20 20:25:32 -0300

    me conocian como Adam

--- TP_final/levelset.c
+++ TP_final/levelset.c
@@ -190,6 +190,10 @@ static int setupLevel(game_state * game){
     (game->pspeedheight)[SAFEROW] = 0;
     (game->pspeedheight)[GOALROW] = 0;
 
+    if(level == 1){ //En el nivel 1 todas las filas comparten velocidad 1, distingo un poco la 3ra hilera de troncos (altura 11) de la 1ra (altura 8)
+        (game->pspeedheight)[11] = ((game->pspeedheight)[8] > 0) ? (game->pspeedheight)[8] + 1 : (game->pspeedheight)[8] - 1;
+    }
+
     for(i = 0; i < 3; i++){
 
         createEnemy(game->penemies, 0, 1); //Los autos y los camiones de la ultima calle
